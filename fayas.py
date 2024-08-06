7976477e7339b6fbf7275cc332e2ddf15951305a
Muhammed-fayas <fayas1676@gmail.com>
2024-08-06 11:29:25 +0530

    Add files via upload

---
+++ fayas.py
@@ -0,0 +1,117 @@
+1
+1+2
+40-10
+id(40-10)
+type(12)
+a=1
+a
+id(12)
+type(34)
+262568*3454521
+b=None
+fayas=20
+fayas
+id(550)
+type(13)
+mr=45
+type(fayas)
+b=True
+b
+id(13)
+b=True
+b
+c=True
+id
+id(b)
+b=18
+type(c)
+c
+print(b)
+11//2
+a=50
+a
+b=50
+b
+id(a)
+id(b)
+a=300
+a
+b=300
+b
+id(a)
+a=30
+a
+Name="fayas"
+Name
+id("fayas")
+print('fayas')
+
+
+
+name= input("enter a value")
+print(name)
+input2=int(input("enter a value"))
+type(input)
+input2=int(input("enter a mark"))
+input2
+type(input)
+input2=float(input("enter a value"))
+bin(10)
+oct(10)
+hex(10)
+set1={12,45,2,12,'marak',3+8}
+set1
+type(set1)
+print(set1)
+dir(set1)
+
+set2={26,45,12,"zero",33}
+set2
+type(set2)
+print(set2)
+dir(set2)
+set2.difference()
+set1.intersection(set2)
+set2.difference(set1)
+set1.difference(set2)
+set1.union(set2)
+set2.union(set1)
+set1.symmetric_difference(set2)
+set1.add(33)
+set1
+set2.add(21)
+set2
+range(1,10)
+range
+list(range(1,10))
+range(1,101)
+range
+x=list(range(0,101,2))
+input5=int(input("enter a value"))
+input5
+type(input5)
+
+
+#conditional stetment
+
+
+if input5%2==0:
+    print("even")
+    
+else:
+  print('odd')
+
+  
+if input5>50:
+   print('greatrthan50')
+elif input5<50:
+   print("lesthan 50")
+else:
+    print("equal to 50")
+
+
+
+
+
+
+
--- fayas1.py
+++ fayas1.py
@@ -1,71 +1,84 @@
-name='fayas'
-name
-id('fayas')
-print('fayas')
-name[0]
-name[1]
-name[2]
-name[3]
-name[4]
-name[:]
-name[0:3]
-name[1:4]
-name[::-1]
-name[::2]
-name[1::2]
-dir(name)
-name.upper()
-name.title()
-name.join("fayas")
-tuple1=1,4,7,"fayas",True,8+6j
-tuple1
-dir("fayas")
-type(tuple)
-tuple1[0:]
-tuple1[1]
-tuple1[2]
-tuple1[3]
-tuple1[4]
-tuple1[5]
-tuple1[0:3]
-tuple1[2:4]
-tuple1[:]
-dir(name)
-name.upper()
-tuple1.title()
-tuple1.count(1)
-dir(tuple1)
-tuple1.index(8+6j)
-
-list1=[1,4,7,"fayas",True,8+6j]
-type(tuple1)
-dir(tuple1)
-tuple1[0:]
-tuple1[1]
-tuple1[2]
-tuple1[3]
-tuple1[4]
-tuple1[5]
-tuple1[:]
-dir(name)
-name.upper()
-tuple1.count(3)
-tuple1
-tuple1[0]
-tuple1[1]
-tuple1[2]
-tuple1[3]
-tuple1[4]
-tuple[5]
-tuple1[3][2:]
-tuple1.append
-tuple1.append(70)
-tuple1
-tuple1.insert(2,5)
-tuple1
+f=(11,12,13,14,15)
+f
+type(f)
+for value in f:
+    print(value)
+list1=[]
+list2=[]
+x=range(1,101)
+for value in x:
+    if value%2==0:
+        list1.append("odd")
+    else:
+        list2.append("even")
+print(list1)
+print(list2)
 
 
-list2=[23,14,36,"neymar",True,4+6j]
-tuple1
-list1.append(list2)
-list1
+for value in x:
+    if value%3==0:
+        print(value)
+
+for value in x:
+    if (value%3==0)|(value%5==0):
+        print(value)
+
+x=range(1,101)
+for value in x:
+    if value==50:
+       continue
+    print(value)
+
+v=range(1,11)
+mlt=1
+for value in v:
+    mlt=mlt*value
+print(mlt)
+
+g=[11,22,444,55,98,56,100,54,105]
+
+min=g[0]
+for value in g:
+    if min<value:
+        pass
+    else:
+        min=value
+print(min)
+
+mlt
+
+
+
+###
+
+
+
+list5=[1,3,2,4,5,6,7,8,9,10,3,6]
+list6=[]
+for value in list5:
+    if list5.count (value)==5:
+        list6.append(value)
+print(list6)
+
+   
+
+   
+list1=[1,3,2,4,5,6,7,8,9,10,3,6]
+list2=[]
+for value in list1:
+    if list1.count (value)==1:
+        list2.append(value)
+print(list2)
+
+   
+   
+
+list3=[1,1,3,3,4,3,1,5,5,6,4,5,8,9,5,7,]
+list4=[]
+for value in list3:
+    if list3.count (value)in[1,2]:
+        list4.append(value)
+print(set[list4])
+
+
+   
